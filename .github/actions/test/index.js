ecfb2003ba02a5b42eb06efb271c235b73049fe7
Farbod <cppox1@gmail.com>
2021-11-18 21:47:22 +0400

    .

--- .github/actions/test/index.js
+++ .github/actions/test/index.js
@@ -1,7 +1,13 @@
 console.log('Hello World!')
 const testFolder = './';
 const fs = require('fs');
-
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 fs.readdirSync(testFolder).forEach(file => {
   console.log(file);
 });
+fs.readFile('index.html', function(err, data) {
+    console.log(data.toString());
+
+
+  });
