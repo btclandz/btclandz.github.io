d90a9ead4bc5535a5b5b991f4c1a2000357bfc00
Farbod <cppox1@gmail.com>
2021-11-22 11:32:06 +0400

    .

--- .github/actions/test/index.js
+++ .github/actions/test/index.js
@@ -43,7 +43,7 @@ fs.readFile('index.html', function(err, data) {
     elements1 = dom.window.document.getElementById('custom_html-11');
     elements1.parentNode.removeChild(elements1);
     elements1 = dom.window.document.getElementById('custom_html-12');
-    elements1.innerHTML='<iframe data-aa="1845539" src="//ad.a-ads.com/1845539?size=300x250" style="width:300px; height:250px; border:0px; padding:0; overflow:hidden; background-color: transparent;" ></iframe>';
+    elements1.innerHTML='<iframe data-aa="1845539" src="//ad.a-ads.com/1845539?size=320x100" style="width:320px; height:100px; border:0px; padding:0; overflow:hidden; background-color: transparent;" ></iframe>';
     /*var ele = dom.window.document.getElementsByClassName('home');
     var ads = dom.window.document.createElement('div');
     ads.innerHTML = '<iframe data-aa="1845539" src="//ad.a-ads.com/1845539?size=970x90" style="width:970px; height:90px; border:0px; padding:0; overflow:hidden; background-color: transparent;" ></iframe>';
